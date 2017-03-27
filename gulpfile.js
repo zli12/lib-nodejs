81133ed8d5b294f02beea2db8db8687169f711fe
Justin Li <zli@ezesoft.com>
2017-03-27 16:57:31 -0400

    Add more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js','src/**/*.js'];
 
@@ -11,4 +12,44 @@ gulp.task('style', function(){
       verbose: true
     }))
     .pipe(jscs());
+});
+
+gulp.task('inject', function() {
+  var wiredep = require('wiredep').stream;
+  var inject = require('gulp-inject');
+
+
+  var options = {
+    bowerJson: require('./bower.json'),
+    directory: './public/lib',
+    ignorePath: '../../public'
+  };
+
+  var injectSrc = gulp.src(['./public/css/*.css', 
+                            './public/js/*.js'], {read: false});
+
+  var injectOptions = {
+    ignorePath: '/public'
+  };
+
+  return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(inject(injectSrc, injectOptions))
+    .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 3000
+    },
+    watch: jsFiles
+  };
+
+  return nodemon(options)
+    .on('restart', function(ev) {
+      console.log('Restarting...');
+    });
 });
